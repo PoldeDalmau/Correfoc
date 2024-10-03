db97664e0355ba35ae7dcc15f0c95ca5d70fd166
pddalmau <pddalmau@BB-JYKTKX3>
2024-10-03 20:36:27 +0200

    added a blender created character

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Correfoc
 This is my first WebGL project using [Three.js](https://threejs.org/). I'm aiming to create a lively crowd of demons celebrating the Correfoc, a fiery Catalan and Valencian tradition where demons wield explosive pitchforks in a pyrotechnic parade. The plan is to animate a few mischievous demons leading the pack, pitchforks ablaze, and—just for fun—occasionally aiming them at people's feet.
 
-I have incorporated autonomous agents that can follow a path using [YUKA](https://mugen87.github.io/yuka/), a JavaScript library for developing Game AI. In addition, the agents wander around the entity that follows the path to make it look like a chaotic crowd is following a set path around the town.
+I have incorporated autonomous agents that can follow a path using [YUKA](https://mugen87.github.io/yuka/), a JavaScript library for developing Game AI. In addition, the agents wander around the entity that follows the path to make it look like a chaotic crowd is following a set path around the town. A navigation mesh is used to ensure the agents do not access restricted areas such as buildings, cars, etc.
 
 # Acknowledgements
 The 3D model of the city is based on "Low Poly City" (https://sketchfab.com/3d-models/low-poly-city-41697300a4c643d089784b8688b2ed2c) by Alessandro.Diamanti (https://sketchfab.com/diamanti.alessandro) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
--- main.js
+++ main.js
@@ -3,11 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as YUKA from 'yuka';
 import { createGraphHelper } from './src/graph_helper';
 import { createConvexRegionHelper } from './src/navmesh_helper';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { MapControls } from 'three/addons/controls/MapControls.js';
-// import { color } from 'three/webgpu';
-// import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
-// import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 // max map: x = 80
 //          z = 25.2
@@ -18,10 +14,17 @@ const scene = new THREE.Scene();
 
 // initialize camera with position
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 55, 9, -14 );
+// camera.position.set( 55, 9, -14 );
+camera.position.set( 69, 19, 8);
 
+const loaderdemon = new GLTFLoader();
 
-
+loaderdemon.load( './low_poly_city/Characters/demon/dimoni.gltf', function ( gltf ) {
+    const model = gltf.scene;
+    model.position.set(10.5, -2, 19.5);
+    // model.position.set(10.5, -3, 19.5);
+	scene.add( gltf.scene );
+});
 // Red cube
 const redCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const redCubeMaterial = new THREE.MeshStandardMaterial({ color: "red", emissive: 'red', emissiveIntensity: 0.7});
@@ -30,80 +33,96 @@ const redCube = new THREE.Mesh(redCubeGeometry, redCubeMaterial);
 redCube.matrixAutoUpdate = false;
 scene.add(redCube)
 //path behavior
-const followPathBehaviour = new YUKA.FollowPathBehavior();
-followPathBehaviour.active = false;
-followPathBehaviour.nextWaypointDistance = 0.5;
-
 const path = new YUKA.Path();
+const followPathBehaviour = new YUKA.FollowPathBehavior(path);
+followPathBehaviour.active = false;
+// followPathBehaviour.nextWaypointDistance = 1;
+const onPathBehavior = new YUKA.OnPathBehavior();
+onPathBehavior.radius = 0.2;
+
 // path.add(new YUKA.Vector3(48, 5, -35));
-path.add(new YUKA.Vector3(-1.3, 0, 8));
+path.add(new YUKA.Vector3(53, -2, -13));
 
 // vehicle that moves the redcube
+
+
+const entityManager = new YUKA.EntityManager();
+// load city 3D model
+const loader = new GLTFLoader();
+// loader.load( './LowPolyCityWithNavmesh.gltf', function ( gltf ) {
+loader.load( './low_poly_city/scene.gltf', function ( gltf ) {
+    const model = gltf.scene;
+    model.position.set(10.5, -2, 19.5);
+    // model.position.set(10.5, -3, 19.5);
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+    console.log('Failed to load gltf model');
+	console.error( error );
+} );
+
+
 const vehicle = new YUKA.Vehicle();
 vehicle.position.copy(path.current());
 
 vehicle.setRenderComponent(redCube, sync);
 
 vehicle.steering.add(followPathBehaviour);
+vehicle.steering.add(onPathBehavior);
 // vehicle.steering.add(obstacleAvoidanceBehavior);
-vehicle.maxSpeed = 3;
+vehicle.maxSpeed = 2;
+// vehicle.maxForce = 200;
+// vehicle.maxTurnRate = 3;
 
-const entityManager = new YUKA.EntityManager();
 entityManager.add(vehicle);
-// load city 3D model
-const loader = new GLTFLoader();
-loader.load( './low_poly_city/scene.gltf', function ( gltf ) {
-	scene.add( gltf.scene );
-}, undefined, function ( error ) {
-	console.error( error );
-} );
-
 
 // load navigation mesh
 const navmeshLoader = new YUKA.NavMeshLoader();
-navmeshLoader.load('./low_poly_city/LowPolyCityNavmesh.glb').then((navigationMesh) =>{
+navmeshLoader.load('./low_poly_city/navmesh/testNavmeshglb.glb').then((navigationMesh) =>{
+// navmeshLoader.load('./low_poly_city/navmesh/LowPolyCityNavmesh.glb').then((navigationMesh) =>{
+// navmeshLoader.load('./NavmeshTest.gltf').then((navigationMesh) =>{
     const navMesh = navigationMesh;
 
     const graph = navMesh.graph;
     const graphHelper = createGraphHelper(graph, 0.2);
-    scene.add(graphHelper);
+    // scene.add(graphHelper);
 
     const navmeshGroup = createConvexRegionHelper(navMesh);
     scene.add(navmeshGroup);
-    // const mousePosition = new THREE.Vector2();
-    // const raycaster = new THREE.Raycaster();
+    const mousePosition = new THREE.Vector2();
+    const raycaster = new THREE.Raycaster();
 
-    let i = 0;
     window.addEventListener('click', function(e) {
-        // mousePosition.x = (e.clientX/this.window.innerWidth) * 2 - 1;
-        // mousePosition.y = (e.clientY/this.window.innerHeight) * 2 + 1;
+        mousePosition.x = (e.clientX/this.window.innerWidth) * 2 - 1;
+        mousePosition.y = -(e.clientY/this.window.innerHeight) * 2 + 1;
 
-        // raycaster.setFromCamera(mousePosition, camera);
-        // const intersects = raycaster.intersectObject(navmesh);
-        // findPathTo(new YUKA.Vector3(63, 5, -70));
-        i++;
-        if (i % 2 == 0){
-            findPathTo(new YUKA.Vector3(-1.3, 0, 8));
+        raycaster.setFromCamera(mousePosition, camera);
+        const intersects = raycaster.intersectObject(navmeshGroup);
+
+        if (intersects.length > 0){
+            findPathTo(new YUKA.Vector3().copy(intersects[0].point));
+            console.log('added point to path');    
         } else {
-            findPathTo(new YUKA.Vector3(-6.7, 0, 14.6));
+            console.log('try another point on the mesh');    
         }
-        console.log('clicked!');
     })
+        function findPathTo(target){
+            const from = vehicle.position;
+            const to = target;
+            const path = navMesh.findPath(from, to);
 
+            const followPathBehaviour = vehicle.steering.behaviors[0];
+            followPathBehaviour.active = true;
+            followPathBehaviour.path.clear();
+            onPathBehavior.path.clear();
 
-    function findPathTo(target){
-        const from = vehicle.position;
-        const to = target;
-        const path = navMesh.findPath(from, to);
-
-        const followPathBehaviour = vehicle.steering.behaviors[0];
-        followPathBehaviour.active = true;
-        followPathBehaviour.path.clear();
-
-        for (let point of path){
-            followPathBehaviour.path.add(point);
+            console.log('points added:', path.length);
+            for (let point of path){
+                followPathBehaviour.path.add(point);
+                onPathBehavior.path.add(point);
+            }
         }
-    }
+
+
 });
 
 
@@ -130,113 +149,21 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 // initialize controls
-// const controls = new OrbitControls( camera, renderer.domElement );
 const controls = new MapControls( camera, renderer.domElement );
 controls.enableDamping = true;
-controls.target.set(46, 1, -31);
-// const controls = new FirstPersonControls( camera, renderer.domElement );
-// const controls = new PointerLockControls( camera, renderer.domElement );
-
-// controls.update();
-// town hall mesh for AI to know obstacle avoidance
-// const townHallGeometry = new THREE.SphereGeometry(3);
-// // const townHallGeometry = new THREE.BoxGeometry(22, 30, 22);
-// townHallGeometry.computeBoundingSphere();
-// const townHallMaterial = new THREE.MeshBasicMaterial();
-// const townHall = new THREE.Mesh(townHallGeometry, townHallMaterial);
-// // townHall.position.set(47, 1, -52);
-// townHall.position.set(45, 5, -30);
-// scene.add(townHall);
-// const obstacleTH = new YUKA.GameEntity();
-// obstacleTH.position.copy(townHall.position);
-// obstacleTH.boundingRadius = townHallGeometry.boundingSphere.radius;
-// entityManager.add(obstacleTH);
-// const obstacleAvoidanceBehavior = new YUKA.ObstacleAvoidanceBehavior([obstacleTH]);
+controls.target.set(53, 0, -13);
 
 
 function sync(entity, renderComponent) {
     renderComponent.matrix.copy(entity.worldMatrix);
 }
 
-// path.add(new YUKA.Vector3(30, 5, -30));
-// path.add(new YUKA.Vector3(60, 5, -30));
-// // path.add(new YUKA.Vector3(48, 5, -35));
-// // path.add(new YUKA.Vector3(40, 5, -35));
-// path.add(new YUKA.Vector3(48, 5, -30));
-// // path.add(new YUKA.Vector3(27, 5, -41));
-// // path.add(new YUKA.Vector3(30, 5, -69));
-// path.add(new YUKA.Vector3(63, 5, -70));
-// path.add(new YUKA.Vector3(66.5, 5, -24));
-// path.loop = true;
-// make a sprite (cube)
-// black cube
-// const seekBehavior = new YUKA.SeekBehavior(vehicle.position);
-// let behaviorFLock = [];
-// for(let i = 0; i < 4; i ++){
-//     // const wanderBehavior = new YUKA.WanderBehavior();
-//     // const wanderBehavior = new YUKA.WanderBehavior(10, .2);
-//     const seekBehavior = new YUKA.OffsetPursuitBehavior(vehicle, new YUKA.Vector3(Math.random() * 3 - 1, 0, Math.random() * 3 - 1), 2);
-//     let blackCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-//     let blackCubeMaterial = new THREE.MeshLambertMaterial({ color: "white"});
-//     let blackCube = new THREE.Mesh(blackCubeGeometry, blackCubeMaterial);
-//     blackCube.position.set(47 - 5 + Math.random() * 3, 5, -32 - 5 + Math.random() * 3);
-//     let vehicleSeeker = new YUKA.Vehicle();
-//     // vehicleSeeker.steering.add(obstacleAvoidanceBehavior);
-//     vehicleSeeker.steering.add(seekBehavior);
-//     behaviorFLock.push(seekBehavior);
-//     // vehicleSeeker.steering.add(wanderBehavior);
-//     vehicleSeeker.position = blackCube.position;
-//     blackCube.matrixAutoUpdate = false;
-//     vehicleSeeker.maxSpeed = 5;
-//     vehicleSeeker.maxForce = 100;
-//     scene.add(blackCube)
-//     vehicleSeeker.setRenderComponent(blackCube, sync);
-//     entityManager.add(vehicleSeeker);
-// }
-
 const time = new YUKA.Time();
-
-// // Adding bounding box to our black box
-// const blackCubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-// blackCubeBB.setFromObject(blackCube);
 
 
 // Adding bounding box to our red box
 const redCubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
 redCubeBB.setFromObject(redCube);
-
-
-let PL_Z = 1;
-pointLight.position.set( 47, PL_Z, -32 );
-
-// document.getElementById("play").onclick = ()=>{
-//     controls.lock();
-// }
-// function checkCollision() {
-//     if (redCubeBB.intersectsBox(blackCubeBB)) {
-//         // console.log('test');
-//         // blackCube.material.opacity = 0.5;
-//         blackCube.material.color = new THREE.Color(0xffffff);
-//     } else {
-//         // blackCube.material.transparent = true;
-//         // blackCube.material.opacity = 0.5;
-//         blackCube.material.color = new THREE.Color(0x000000);
-//     }
-// }
-// // Adding event listener to keyPressed event and changing position of red cube
-// document.addEventListener("keydown", onDocumentKeyDown, false);
-// function onDocumentKeyDown(event) {
-//     var keyCode = event.which;
-//     if (keyCode == 87) { // up
-//         redCube.position.x -= .3;
-//     } else if (keyCode == 83) { // down
-//         redCube.position.x += .3;
-//     } else if (keyCode == 65) { // left
-//         redCube.position.z += .3;
-//     } else if (keyCode == 68) { // right
-//         redCube.position.z -= .3;
-//     }
-// }
 
 function animate() {
     // set new point:
@@ -256,50 +183,15 @@ function animate() {
     // PL_Z += 0.1;
     renderer.render( scene, camera );
     // console.log("cam XYZ: ", camera.position.x, camera.position.y, camera.position.z)
-    // console.log("redCube XYZ: ", vehicle.position.x, vehicle.position.y, vehicle.position.z)
+    console.log("redCube XYZ: ", vehicle.position.x, vehicle.position.y, vehicle.position.z)
     pointLight.position.copy(vehicle.position); //.add(new THREE.Vector3(0,1,0));
     // pointLight.intensity = 50 + 10 * Math.random();
 
 
     let a = new YUKA.Vector3();
-    // console.log(a.copy(behaviorFLock[0].offset).add(vehicle.position));
 
 }
 
 // animate();
 
 renderer.setAnimationLoop(animate);
-
-
-// const restrictedAreas = [];
-// restrictedAreas.push([[35, -41], [57.7, -62]]); // townhall
-// // restrictedAreas.push([[35, -41], [57.7, -62]]); // other
-// const currpostest = [30, -42];
-// const targettest = [37, -43];
-// function checkIfRestricted(currentPos, target, restrictedAreas){
-//     //only takes arrays containing two points defining an aabb
-//     const p0 = currentPos;
-//     const p1 = target;
-//     // iterate through all restricted areas
-
-//     for(let areaInd = 0; areaInd < restrictedAreas.length; areaInd++){
-//         const p2 = restrictedAreas[areaInd][0];
-//         const p3 = [restrictedAreas[areaInd][0][0], restrictedAreas[areaInd][1][1]];
-
-//         const s1 = [0, 0];
-//         const s2 = [0, 0];
-//         s1[0] = p1[0] - p0[0];
-//         s1[1] = p1[1] - p0[1];
-//         s2[0] = p3[0] - p2[0];
-//         s2[1] = p3[1] - p2[1];
-
-//         let s, t;
-//         s = (-s1[1] * (p0[0] - p2[0]) + s1[0] * (p0[1] - p2[1])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-//         t = ( s2[0] * (p0[1] - p2[1]) - s2[1] * (p0[0] - p2[0])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-
-//         if (s >= 0 && s <= 1 && t >= 0 && t <= 1){return true;}
-
-//     }
-//     return false; // no intersection
-// }
-// console.log(checkIfRestricted(currpostest, targettest, restrictedAreas));
